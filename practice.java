71f674ac3be3789d6af9eca6a49477a33ada658e
geumjoon <152842614+Funital@users.noreply.github.com>
2025-04-14 21:00:46 +0900

    fix: practice.java 수정
    #1

--- practice.java
+++ practice.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class practice {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.next());
+        System.out.println(a);
     }
 }
