3c211c667e38867283950655b87b1c3c98462777
DemonOfCoding <kcm9561@naver.com>
2025-05-07 18:44:10 +0900

    feat : JPA 매핑 및 기본 세팅 #10
    모든 엔티티 매핑 및 리포지토리 생성

---
+++ src/main/java/study/goorm/GoormApplication.java
@@ -0,0 +1,14 @@
+package study.goorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class GoormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormApplication.class, args);
+    }
+}
