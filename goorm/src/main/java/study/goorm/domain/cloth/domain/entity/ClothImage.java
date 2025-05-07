613d43c3d0f6c6664f657312edea5e7cc7401a06
JuHyeonAh <jha08130930@gmail.com>
2025-05-07 18:54:52 +0900

    ClothImage Mapping

--- goorm/src/main/java/study/goorm/domain/cloth/domain/entity/ClothImage.java
+++ goorm/src/main/java/study/goorm/domain/cloth/domain/entity/ClothImage.java
@@ -1,6 +1,25 @@
 package study.goorm.domain.cloth.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
 import study.goorm.domain.model.entity.BaseEntity;
 
-public class ClothImage {
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ClothImage extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 옷 사진 ID
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cloth_id", nullable = false)
+//    @JoinColumn(name = "cloth_id", nullable = false, unique = true)
+    private Cloth cloth;
+
+    @Column(nullable = false)
+    private String imageUrl;
 }
