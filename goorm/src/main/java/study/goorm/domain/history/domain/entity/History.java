820af35f469c8da58e1cf90e4e36deccb11ac4e2
JuHyeonAh <jha08130930@gmail.com>
2025-05-07 18:08:27 +0900

    History Mapping

--- goorm/src/main/java/study/goorm/domain/history/domain/entity/History.java
+++ goorm/src/main/java/study/goorm/domain/history/domain/entity/History.java
@@ -20,19 +20,21 @@ import java.time.LocalDate;
 public class History extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
-    private LocalDate historyDate;
-
-    @Min(0) // 0이상만 허용 (유효성 검사)
-    @Column(nullable = false, columnDefinition = "integer default 0") // DB 저장 시 기본 값 0으로 설정)
-    private int likes;
-
-    @Column(length = 100)
-    private String content;
+    private Long id; // 기록 ID
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
-    private Member member;
+    private Member member; // 사용자 ID
+
+    @Column(nullable = false)
+    private LocalDate historyDate; // 날짜
+
+    @Min(0) // 0 이상만 허용 (유효성 검사)
+    @Column(nullable = false, columnDefinition = "integer default 0") // DB 저장 시 기본 값 0으로 설정)
+    private int likes; // 좋아요 수
+
+    @Column(length = 100)
+    private String content; // 내용
+
+
 }
