04248b8eb83b913ebdcacc970c6d086de45c6e3e
JuHyeonAh <jha08130930@gmail.com>
2025-05-14 18:07:29 +0900

    Base Response

--- goorm/src/main/java/study/goorm/global/common/response/BaseResponse.java
+++ goorm/src/main/java/study/goorm/global/common/response/BaseResponse.java
@@ -1,11 +1,19 @@
 package study.goorm.global.common.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-@JsonPropertyOrder({"isSu"})
-public class BaseResponse {
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class BaseResponse<T> { // 어떤 타입이든 result로 받도록 설계
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL) // null이면 JSON 응답에 포함되지 않도록 설정됨
+    private final T result; // 실제 반환하고 싶은 데이터
 }
