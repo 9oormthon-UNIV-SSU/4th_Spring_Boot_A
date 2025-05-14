91f0807ffe3a0d81e6b7a37ce9ee451254d9e254
JuHyeonAh <jha08130930@gmail.com>
2025-05-14 18:22:51 +0900

    기초 코드

--- goorm/src/main/java/study/goorm/global/error/code/status/SuccessStatus.java
+++ goorm/src/main/java/study/goorm/global/error/code/status/SuccessStatus.java
@@ -1,4 +1,16 @@
 package study.goorm.global.error.code.status;
 
-public class SuccessStatus {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus {
+    OK(HttpStatus.OK, "COMMON_200", "성공입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
 }
