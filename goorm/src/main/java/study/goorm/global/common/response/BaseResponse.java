c0158b8e3a8a16a75097a7b6124345affa554a7b
JuHyeonAh <jha08130930@gmail.com>
2025-05-14 18:39:00 +0900

    BaseResponse 완성

--- goorm/src/main/java/study/goorm/global/common/response/BaseResponse.java
+++ goorm/src/main/java/study/goorm/global/common/response/BaseResponse.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import study.goorm.global.error.code.status.ErrorStatus;
+import study.goorm.global.error.code.status.SuccessStatus;
 
 @Getter
 @AllArgsConstructor
@@ -16,4 +18,20 @@ public class BaseResponse<T> { // 어떤 타입이든 result로 받도록 설계
     private final String message;
     @JsonInclude(JsonInclude.Include.NON_NULL) // null이면 JSON 응답에 포함되지 않도록 설정됨
     private final T result; // 실제 반환하고 싶은 데이터
+
+    public static <T> BaseResponse<T> onSuccess(SuccessStatus status, T result){
+        return new BaseResponse<>(
+                true,
+                status.getCode(),
+                status.getMessage(),
+                result);
+    }
+
+    public static <T> BaseResponse<T> onFailure(ErrorStatus status, T result){
+        return new BaseResponse<>(
+                false,
+                status.getCode(),
+                status.getMessage(),
+                result);
+    }
 }
